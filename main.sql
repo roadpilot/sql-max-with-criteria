e3d96536483c879a99f1fac7cab8cedfef1c1114
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-09-27 16:14:09 +0000

    order to test for max value

--- main.sql
+++ main.sql
@@ -1,4 +1,4 @@
 /*
 Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345  Truncate your answer to 4 decimal places.
 */
-select lat_n from station where lat_n < 137.2345
+select lat_n from station where lat_n < 137.2345 order by lat_n desc
